29dd1f3475782a50e9fdc79381b3a01490497051
kpedersen <kpedersen@z420.local>
2023-06-22 22:30:18 +0000

    Patch around the pledge/unveil firefox stuff.

--- README.txt
+++ README.txt
@@ -8,15 +8,14 @@ vlc (tested 3.0.18)
 gcc (tested 11.2.0p6) - gcc fetches additional g++, gmake, gdb
 git (tested 2.40.0)
 
-Terrible Hack
--------------
 blender (tested 3.0.1p1) - patch - LD_LIBRARY_PATH ignored in some libs
 libreoffice (tested 7.5.1.2v0) - patch - icu4c has ICU_DATA disabled
 chromium (tested 111.0.5563.110) - patch - icu4c has ICU_DATA disabled
+firefox (tested 111.0) - patch - disable pledge/unveil
 
 Partially working
 -----------------
-firefox (tested 111.0) - pledge - Requires manual tweaks, read start script
+none
 
 # NOTES
 Scan libs using objdump instead of ldd or hardcoding (i.e blender)
--- hacks/firefox/post.sh
+++ hacks/firefox/post.sh
@@ -1,3 +1,18 @@
 cd "${prefix}"
 cc -opatch-icu "${standalone_prefix}/hacks/patch-icu.c"
 ./patch-icu
+
+cc -opatch-unveil "${standalone_prefix}/hacks/patch-unveil.c"
+./patch-unveil
+
+mkdir etc/firefox
+echo "disable" > etc/firefox/pledge.main
+echo "disable" > etc/firefox/pledge.content
+echo "disable" > etc/firefox/pledge.socket
+echo "disable" > etc/firefox/pledge.rdd
+echo "disable" > etc/firefox/pledge.utility
+echo "disable" > etc/firefox/unveil.main
+echo "disable" > etc/firefox/unveil.content
+echo "disable" > etc/firefox/unveil.utility
+echo "disable" > etc/firefox/unveil.socket
+echo "disable" > etc/firefox/unveil.rdd
